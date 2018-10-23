b846f472c6eba636a4c846f8e1f5e8b7b70931a0
Kapitun, Dmitriy [dkapitun] <dkapitun@deloitte.ru>
2018-10-23 19:21:49 +0300

    Fix accounts not persisted

--- src/main/scala/repository/AccountRepository.scala
+++ src/main/scala/repository/AccountRepository.scala
@@ -8,17 +8,20 @@ import model.Info.Uuid
 import scala.collection.mutable
 
 abstract class AccountRepository extends Repository[AccountInfo] {
-  abstract def createAccount: AccountInfo
+  def createAccount: AccountInfo
 }
 
 class AccountInMemoryRepository extends AccountRepository {
-  val accounts: mutable.Map[Uuid, AccountInfo] = mutable.Map[Uuid, AccountInfo]()
+  var accounts: mutable.Map[Uuid, AccountInfo] = mutable.Map[Uuid, AccountInfo]()
 
   override def findById(id: Uuid): Option[AccountInfo] = accounts get id
 
   override def findAll: Iterable[AccountInfo] = accounts.values
 
-  override def save(entity: AccountInfo): AccountInfo = accounts + entity.id -> entity _2
+  override def save(entity: AccountInfo): AccountInfo = {
+    accounts += entity.id -> entity
+    entity
+  }
 
   override def update(entity: AccountInfo): AccountInfo = {
     accounts.update(entity.id, entity)
